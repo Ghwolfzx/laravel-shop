a812bf06726dad3c3c664fc3200b3ff1e15479a4
任中兴 <ghwolf@foxmail.com>
2019-01-14 11:09:11 +0800

    众筹逻辑结束

---
+++ app/Jobs/RefundCrowdfundingOrders.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\CrowdfundingProduct;
+use App\Models\Order;
+use App\Services\OrderService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class RefundCrowdfundingOrders implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $crowdfunding;
+
+    public function __construct(CrowdfundingProduct $crowdfunding)
+    {
+        $this->crowdfunding = $crowdfunding;
+    }
+
+    public function handle()
+    {
+        // 如果众筹状态不是失败则不执行退款
+        if ($this->crowdfunding->status !== CrowdfundingProduct::STATUS_FAIL) {
+            return;
+        } 
+
+        $orderService = app(OrderService::class);
+        // 查询所有参与此众筹的订单
+        Order::query()
+            ->where('type', Order::TYPE_CROWDFUNDING)
+            ->whereNotNull('paid_at')
+            ->whereHas('items', function ($query) use ($crowdfunding) {
+                // 包含当前商品
+                $query->where('product_id', $crowdfunding->product_id);
+            })
+            ->get()
+            ->each(function (Order $order) use ($orderService) {
+                $orderService->refundOrder($order);
+            });
+    }
+}
