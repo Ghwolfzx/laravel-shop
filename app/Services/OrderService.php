fa7e3c9354f8a918037721c02538a4fe04e81ffb
Ghwolf <ghwolf@foxmail.com>
2019-01-15 14:52:31 +0800

    分期付款退款

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -11,6 +11,7 @@ use App\Exceptions\InternalException;
 use App\Jobs\CloseOrder;
 use Carbon\Carbon;
 use App\Models\CouponCode;
+use App\Jobs\RefundInstallmentOrder;
 use App\Exceptions\CouponCodeUnavailableException;
 
 class OrderService
@@ -183,6 +184,14 @@ class OrderService
                         'refund_status' => Order::REFUND_STATUS_SUCCESS,
                     ]);
                 }
+                break;
+            case 'installment':
+                $order->update([
+                    'refund_no' => Order::getAvailableRefundNo(), // 生成退款订单号
+                    'refund_status' => Order::REFUND_STATUS_PROCESSING, // 将退款状态改为退款中
+                ]);
+                // 触发退款异步任务
+                dispatch(new RefundInstallmentOrder($order));
                 break;
             default:
                 // 原则上不可能出现，这个只是为了代码健壮性
